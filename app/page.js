da67c917199df26ab1e1da2bc00dfba46b8113d9
Nasim Md Emon <nasimmdemon@Nasims-MacBook-Air.local>
2024-06-23 02:18:16 +0600

    Second changes done

--- app/page.js
+++ app/page.js
@@ -7,44 +7,17 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Launching Soon</title>
+        <title>UNDER UTVECKLING</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <div className={styles.content}>
-          <h1 className={styles.title}>Launching soon</h1>
-          <p className={styles.description}>
-            Tech doesn’t have to feel like a different language. We built
-            Schematiq to make sure innovation works for you, your business, and
-            your people. We can’t wait to innovate with you!
-          </p>
+          <h1 className={styles.title}>UNDER UTVECKLING</h1>
+          <p className={styles.description}>SNART ÖPPNAR VI</p>
           <button className={styles.contactButton}>
-            <a href="mailto:teltwebb@gmail.com">Contact Us</a>
+            <a href="mailto:teltwebb@gmail.com">Kontakta oss</a>
           </button>
-          <div className={styles.social}>
-            <a
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Facebook
-            </a>
-            <a
-              href="https://twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Twitter
-            </a>
-            <a
-              href="https://instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Instagram
-            </a>
-          </div>
         </div>
         <div className={styles.imageContainer}>
           <Image
